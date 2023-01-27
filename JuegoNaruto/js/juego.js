ae455412b0d418e37e00ee39a82697519dc8c4bd
Maria Fernanda Echeverria Colorado <fernanda.ecmf@gmail.com>
2023-01-26 21:01:52 -0600

    muchos cambios!

--- JuegoNaruto/js/juego.js
+++ JuegoNaruto/js/juego.js
@@ -1,3 +1,5 @@
+
+//Seleccion de Canvas
 const canvas = document.getElementById("juego")
 console.log(canvas)
 
@@ -10,6 +12,8 @@ const menu = document.querySelector(".botones")
 //Seleccion GameOver
 const gameOver = document.querySelector(".gameOver")
 
+//Seleccion Win
+const winGame= document.querySelector(".winGame")
 
 //Cargar imagenes
 
@@ -17,7 +21,12 @@ const heart = new Image()
 heart.src = "./assets/heart.png"
 console.log(heart)
 
-//prueba de avatar
+//Prueba avatar Moneda
+const itachi= new Image()
+itachi.src = "./assets/moneda.png"
+console.log(itachi)
+
+//Prueba de avatar
 const naruto = new Image()
 naruto.src = "./assets/narutopre.png"
 console.log(naruto)
@@ -43,12 +52,48 @@ naveD.src = "./assets/naruto1.png"
 const naveI = new Image()
 naveI.src= "./assets/naruto5.png"
 
+// Insignia de Cuervo
+
+const coin0 = new Image()
+coin0.src= "./assets/coin0.png"
+
+const coin1 = new Image()
+coin1.src= "./assets/coin1.png"
+
+const coin2 = new Image()
+coin2.src= "./assets/coin2.png"
+
+const coin3 = new Image()
+coin3.src= "./assets/coin3.png"
+
+const coin4 = new Image()
+coin4.src= "./assets/coin4.png"
+
+const coin5 = new Image()
+coin5.src= "./assets/coin5.png"
+
+const coin6 = new Image()
+coin6.src= "./assets/coin6.png"
+
+const coin7 = new Image()
+coin7.src= "./assets/coin7.png"
+
+const coin8 = new Image()
+coin8.src= "./assets/coin8.png"
+
+const coin9 = new Image()
+coin9.src= "./assets/coin9.png"
+
+const coins= [coin0,coin1,coin2,coin3,coin4,coin5,coin6, coin7,coin8, coin9]
+
+
 
 //Sonidos
 //play()
 const shoot = new Audio("./sounds/sonido-Rasengan.mp3")
 const die = new Audio("./sounds/sonido-Perdiste.mp3")
 const pelea = new Audio("./sounds/sonido-pelea.mp3")
+const ganaste = new Audio("./sounds/sonido-ganaste.mp3")
 
 
 //ARREGLO aliens
@@ -58,12 +103,14 @@ const tiposAliens = [alien, alien1, alien2, alien3]
 //ARREGLO Balas
 const balas = []
 
-//Alien Arreglo
+//ARREGLO Aliens
 const aliens = []
 
+//ARREGLO moneda
+const monedas= []
 
     //Personaje - clase 
-class Nave {
+    class Nave {
     constructor(x,y,w,h){
         this.x = x
         this.y = y
@@ -73,11 +120,12 @@ class Nave {
         this.kills = 0
         this.lifes = 3
         this.img = naveD
+        this.coins = 0
+    
     }
-
+    
     //Metodos de Personaje
  
-
     //drawImage
     dibujarse(){
         ctx.drawImage(this.img, this.x, this.y, this.w, this.h)
@@ -92,49 +140,30 @@ class Nave {
         shoot.play()
     }
 
-    /* Valor + en X*/
+    /* Valor + y - en X*/
 
     adelante(){
-
         if(this.x < 515){
-
-        this.x += this.velocidad
-
-        }
-        this.img = naveD
-    }
-    /* Valor - en X*/
-
+        this.x += this.velocidad}
+        this.img = naveD}
     atras(){
-
-       if (this.x > 0){
-
-            this.x -= this.velocidad
-        }
-                 this.img = naveI
+    if (this.x > 0){
+    this.x -= this.velocidad }
+    this.img = naveI
     }
 
-    /* Valor - en Y */
+    /* Valor + y - en Y */
 
     arriba(){
-
-        if (this.y > 20) {
-        
-            this.y -= this.velocidad
-
-        }
+    if (this.y > 20) {
+    this.y -= this.velocidad}
     }
 
-    /* Valor + en Y */
 
     abajo(){
-        
-        if(this.y < 215){
-
-        this.y += this.velocidad
+    if(this.y < 215){
+    this.y += this.velocidad}    
     }
-
-}
 
 }
     //Disparar - clase
@@ -143,44 +172,54 @@ class Nave {
     constructor(x, y){
         this.x = x; 
         this.y = y;
-      
-
-     }
+                    }
         dibujarse(){
            ctx.drawImage(bullet,this.x, this.y, 50, 50) 
            this.x += 20 // va hacia adelante por el eje de X +
            if(this.x > 600){
-            balas.shift()
-           }
-
+            balas.shift()}
         }
-
-
-
-      
     }
 
     
-     //Alien clase
-
+     //Alien - clase
      class Alien{
     constructor(x, y, img){
         this.x = x;
         this.y = y;
-        this.img = img;
-
-    }
+        this.img = img;}
     dibujarse(){
         this.x -= 7
         ctx.drawImage(this.img, this.x, this.y, 60, 55) // tamano itachi
     }
 }
-ctx.fillStyle = "white"
+    //Moneda - clase
 
 
 
 
-    //Instancia de personaje
+class Coin{
+    constructor(x){
+        this.x = x
+        this.y = 0
+        this.img= 0
+    }
+
+        dibujarse(){
+            this.y++ //aumenta uno para bajar
+            
+            ctx.drawImage(coins[this.img], this.x, this.y, 55, 50)
+            if (this.img < coins.length -1){
+                this.img++
+        
+            } else {
+                this.img = 0
+            }
+        }
+
+    }      
+    
+//Instancia de personaje
 const nave = new Nave(10,145, 90, 85)// Instancia nave //clase Nave //tamano naruto
     console.log(nave)
 
@@ -202,7 +241,9 @@ document.addEventListener("keydown", (evento) => {
             nave.abajo()
                 break;
                 case " ":
+                    if(balas.length < 3){
             nave.disparar()
+                    }
             break;
     }
 })
@@ -222,12 +263,16 @@ function empezarJuego(){
         }
 
 
-                // //Verificar si gano
-                // if(nave.coins > 5)
-                //  //setWin()
-                //  alert() / // cuervo itachi
+        //Ganar 
+        if(nave.coins === 1){
+            setwinGame()
+        }
 
-   
+
+
+
+
+
             //Dibujar balas
             balas.forEach((bala, indexBala) => {
                 bala.dibujarse()
@@ -237,7 +282,13 @@ function empezarJuego(){
                 aliens.forEach((alien, indexAlien) => {
                     console.log({balaX: bala.x, balaY: bala.y, alienX: alien.x, alienY: alien.y})
 
-                    if (alien.x <= bala.x + 60 && bala.y >= alien.y && bala.y <= alien.y + 55) {
+                    if (
+                        alien.x <= bala.x + 60 &&
+                         bala.y >= alien.y && 
+                         bala.y <= alien.y + 55 &&
+                         bala.x <= alien.x 
+                         ) {
+
                         aliens.splice(indexAlien, 1)
                         balas.splice(indexBala, 1)
                         nave.kills++
@@ -246,24 +297,58 @@ function empezarJuego(){
 
                 })
 
-               
+
+
+
+
 
                 
             })
 
-                                    tiempo++
-                                    ctx.font = "25px Arial"
-                                    ctx.fillText(tiempo,10,30)
+            //Mostrar Monedas
+            monedas.forEach((moneda, indexMoneda)=>{
+                moneda.dibujarse()
+                if(moneda.y >= 300){
+                    monedas.splice(indexMoneda, 1)
+                }
 
-                // Pintar Muertos
-                ctx.fillText(`${nave.kills} Muertos`, 290,30) 
+            // Colision vs Nave 
+                if(moneda.x <= nave.x + 85
+                    && nave.y +85 >= moneda.y 
+                    && nave.x <= moneda.x 
+                    && nave.y <= moneda.y +50){
+                    nave.coins++
+                    monedas.splice(indexMoneda, 1)
+                }
 
-            //Pintar avatar
-            mostarAvatar()
+
+            })
+            
+
+tiempo++
+ctx.font = "bold 25px Arial, sans-serif"
+ctx.fillStyle = "black"
+
+ctx.fillText(tiempo,10,30)
+
+// Pintar Muertos
+    ctx.fillText(`${nave.kills} Muertos`, 285,25) 
+
+//Pintar avatar
+mostarAvatar()
         
-                //Pintar vidas
+//Pintar vidas
                 
-                mostrarVidas()
+mostrarVidas()
+
+
+
+//Pintar Avatar de Monedas
+mostrarMonedas()
+
+//Pintar monedas
+ctx.fillText(`${nave.coins} Insignias`, 110,25)
+
 
  //Dibujar Aliens
     aliens.forEach((alien, indexAlien)=>{
@@ -284,26 +369,22 @@ function empezarJuego(){
     }
 })
    
-    },100)
+    },70) //tiempo en que aparecen los enemigos
 
 
     }
 
-
+   
 //Seleccionamos el boton y empieza el juego
 let btn = document.getElementById("jugar")
     btn.addEventListener("click",() => { 
         empezarJuego()
         crearAliens()
+        crearMoneda()
         btn.classList.add("none")
        
-        //Alien
-
-       
-
 
     })
-
 
     //Creacion de aliens
     function crearAliens(){
@@ -318,11 +399,29 @@ let btn = document.getElementById("jugar")
     }, 2005)
 }
 
-//Mostrar vidas
 
-    function mostarAvatar(){
-        ctx.drawImage(naruto,420,5,30,30)
+////mostrar moneda
+function mostrarMonedas(){
+    ctx.drawImage(itachi, 55, -10 ,50,50)
     }
+
+
+//Creacion de cuervos
+function crearMoneda(){
+    setInterval(()=>{
+        const posicionAleatoria = Math.floor(Math.random()* 570)
+        const moneda = new Coin(posicionAleatoria)
+        monedas.push(moneda)
+    }, 5000)
+}
+
+//Mostrar vidas y Avatar
+
+function mostarAvatar(){
+ctx.drawImage(naruto,410,-12,50,56)
+    }
+
+
 
 
 function mostrarVidas(){
@@ -341,7 +440,7 @@ if (nave.lifes === 3) {
     ctx.drawImage(heart, 460, 10, 30, 30)
 }
 
-    
+//Seccion de Game Over  
 }
 //Game Over
 function setGameOver(){
@@ -350,4 +449,13 @@ function setGameOver(){
     menu.classList.add("none")
     gameOver.classList.remove("none")
     die.play()
+
+
+//Win     
+}
+function setwinGame(){
+    canvas.classList.add("none")
+    menu.classList.add("none")
+    winGame.classList.remove("none")
+    ganaste.play()
 }
